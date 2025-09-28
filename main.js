5ed4cf833857dc39bc9347dea0990a4f1cc6e177
arnavrealcoder <arnath.gautam.1515@gmail.com>
2025-09-28 20:56:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var person = {
+    name: "Adolf",
+    age: 99,
+    grade: 7,
+    hobbies: ["Chess","Piano","Sleeping"],
+  }
+  console.log(person.hobbies[2])
+
+
+  
---
+++ main2.js
@@ -0,0 +1,9 @@
+var value = Math.abs(-67)
+console.log(value)
+
+var values = Math.max(4,3,2,4,5,6,4,3,2,5,6,7,4,5,6,3,2,3,6,8,1,4,5,3,4)
+console.log(values)
+var valuess = Math.sqrt(625)
+console.log(valuess)
+var balues = Math.pow(5594539754897534778345735984357898753498793458795348945387954879548754357894,4089384789274927489274802741747108)
+console.log(balues)
